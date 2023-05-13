82bd553fbe4eedb2d407f08d73a39f7296e55d1e
Bilal Kuzey <bk1405bgl@gmail.com>
2023-05-13 20:51:44 +0200

    added style.css and created a basic html5 template

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
-<?php
-	echo "Hello World";
-?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>My First PHP Page</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<h1>My First PHP Page</h1>
+	<?php
+		echo "Hello World";
+	?>
+
+</body>
+</html>
+
+
